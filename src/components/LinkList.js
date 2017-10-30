648746ca3d9e4220016513cf224f2f4ca15d9d56
Rafael Villarreal <rvillarreal416@gmail.com>
2017-10-30 01:44:44 -0500

    query and mutations works

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,30 +1,48 @@
 import React, { Component } from 'react'
+import { graphql, gql } from 'react-apollo'
 import Link from './Link'
 
-export default class LinkList extends Component {
+class LinkList extends Component {
 
   render() {
 
-    const linksToRender = [
-      {
-        id: '1',
-        description: 'The Coolest GraphQL Backend 😎',
-        url: 'https://www.graph.cool'
-      },
-      {
-        id: '2',
-        description: 'The Best GraphQL Client',
-        url: 'http://dev.apollodata.com/'
-      }
-    ]
-
-    return (
-      <div>
-        {linksToRender.map(link => (
-          <Link key={link.id} link={link}/>
-        ))}
-      </div>
-    )
+  // 1
+  if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
+    return <div>Loading</div>
   }
 
+  // 2
+  if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
+    return <div>Error</div>
+  }
+
+  // 3
+  const linksToRender = this.props.allLinksQuery.allLinks
+
+  return (
+    <div>
+      {linksToRender.map(link => (
+        <Link key={link.id} link={link}/>
+      ))}
+    </div>
+  )
 }
+
+}
+
+
+// 1
+const ALL_LINKS_QUERY = gql`
+  # 2
+  query AllLinksQuery {
+    allLinks {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
+
+// 3
+export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' }) (LinkList)
