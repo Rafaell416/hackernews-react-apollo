32a28a7583dc936eaea7723bedae73379c1d70fd
Rafael Villarreal <rvillarreal416@gmail.com>
2017-10-30 17:08:49 -0500

    subscription endpoint

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 import CreateLink from './CreateLink'
+import LinkList from './LinkList'
+import Header from './Header'
+import { Switch, Route } from 'react-router-dom'
 
 export default class App extends Component {
-  render() {
+    render() {
     return (
-      <CreateLink />
+      <div className='center w85'>
+        <Header />
+        <div className='ph3 pv1 background-gray'>
+          <Switch>
+            <Route exact path='/' component={LinkList}/>
+            <Route exact path='/create' component={CreateLink}/>
+          </Switch>
+        </div>
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,24 +3,49 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import './styles/index.css'
-// 1
+import { BrowserRouter } from 'react-router-dom'
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+import { GC_AUTH_TOKEN  } from './constants'
 
-// 2
+
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj9dlm2rm1kci01763kbzg2v5'
 })
 
-// 3
-const client = new ApolloClient({
-  networkInterface
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj9dlm2rm1kci01763kbzg2v5', {
+  reconnect: true,
+  connectionParams: {
+     authToken: localStorage.getItem(GC_AUTH_TOKEN),
+  }
 })
 
-// 4
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+)
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {}
+    }
+    const token = localStorage.getItem(GC_AUTH_TOKEN)
+    req.options.headers.authorization = token ? `Bearer ${token}` : null
+    next()
+  }
+}])
+
+const client = new ApolloClient({
+  networkInterface: networkInterfaceWithSubscriptions
+})
+
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
   , document.getElementById('root')
 )
 registerServiceWorker()
